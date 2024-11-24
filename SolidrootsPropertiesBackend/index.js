ff848037c23c8b73e1bb10034a3aea1fb48c08b6
Olivia Adongo <akinyioliviatove@gmail.com>
2024-11-24 19:14:09 +0300

    Milestone: Pause

--- SolidrootsPropertiesBackend/index.js
+++ SolidrootsPropertiesBackend/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server listening on port 30000");
-});
 app.get("/", (req, res) => {
-    res.send("Hello from SolidRootsBackend Server server updated");
+  res.send("Hello from SolidRootsBackend Server server updated first");
+});
+
+mongoose
+  .connect(
+    "mongodb://adongoolivia0698:0MlHGDCtvn1k7xXp@backenddatab-shard-00-00.twijb.mongodb.net:27017,backenddatab-shard-00-01.twijb.mongodb.net:27017,backenddatab-shard-00-02.twijb.mongodb.net:27017/?ssl=true&replicaSet=atlas-endxfc-shard-0&authSource=admin&retryWrites=true&w=majority&appName=BackendDataB"
+  )
+  .then(() => {
+    console.log("Connected to SolidRootsProperties Backend Successfully!!");
+  })
+  .catch(() => {
+    console.log("Not Connected Succesfully!!!");
   });
-  
+
