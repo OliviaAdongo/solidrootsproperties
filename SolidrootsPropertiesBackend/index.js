c50d8aa2ecc7c420c632b2707cf48913117db026
Olivia Adongo <akinyioliviatove@gmail.com>
2024-11-24 17:52:30 +0300

    Milestone: Backend Index file

--- SolidrootsPropertiesBackend/index.js
+++ SolidrootsPropertiesBackend/index.js
@@ -1,1 +1,10 @@
-console.log("My server");
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server listening on port 30000");
+});
+app.get("/", (req, res) => {
+    res.send("Hello from SolidRootsBackend Server server updated");
+  });
+  
